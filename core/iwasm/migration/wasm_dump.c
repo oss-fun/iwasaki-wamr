414521fa1828388f26d75d632d5586a5d1961cab
funera1 <funeral7776@gmail.com>
2024-12-08 13:54:13 +0900

    refactor

--- core/iwasm/migration/wasm_dump.c
+++ core/iwasm/migration/wasm_dump.c
@@ -83,100 +83,6 @@ int debug_function_opcodes(WASMModuleInstance *module, WASMFunctionInstance* fun
     return 0;
 }
 
-// int debug_flag = 0;
-// ipからip_limまでにopcodeがいくつかるかを返す
-// int get_opcode_offset(uint8 *ip, uint8 *ip_lim) {
-//     uint32 cnt = 0;
-//     bh_assert(ip != NULL);
-//     bh_assert(ip_lim != NULL);
-//     bh_assert(ip <= ip_lim);
-//     if (ip > ip_lim) return -1;
-//     if (ip == ip_lim) return 0;
-//     while (1) {
-//         // LOG_DEBUG("get_opcode_offset::ip: 0x%x\n", *ip);
-//         // if (debug_flag) {
-//         //     printf("(cnt, opcode) = (%d, 0x%x)\n", cnt, *ip);
-//         // }
-//         ip = dispatch(ip, ip_lim);
-//         cnt++;
-//         if (ip >= ip_lim) break;
-//     }
-//     return cnt;
-// }
-
-// TODO: コードごちゃごちゃで読めないので、整理する
-// uint8* get_type_stack(uint32 fidx, uint32 offset, uint32* type_stack_size, bool is_return_address) {
-//     FILE *tablemap_func = fopen("tablemap_func", "rb");
-//     if (!tablemap_func) printf("not found tablemap_func\n");
-//     FILE *tablemap_offset = fopen("tablemap_offset", "rb");
-//     if (!tablemap_func) printf("not found tablemap_offset\n");
-//     FILE *type_table = fopen("type_table", "rb");
-//     if (!tablemap_func) printf("not found type_table\n");
-    
-//     /// tablemap_func
-//     fseek(tablemap_func, fidx*sizeof(uint32)*3, SEEK_SET);
-//     uint32 ffidx;
-//     uint64 tablemap_offset_addr;
-//     fread(&ffidx, sizeof(uint32), 1, tablemap_func);
-//     if (fidx != ffidx) {
-//         perror("tablemap_funcがおかしい\n");
-//         exit(1);
-//     }
-//     fread(&tablemap_offset_addr, sizeof(uint64), 1, tablemap_func);
-
-//     /// tablemap_offset
-//     fseek(tablemap_offset, tablemap_offset_addr, SEEK_SET);
-//     // 関数fidxのローカルを取得
-//     uint32 locals_size;
-//     fread(&locals_size, sizeof(uint32), 1, tablemap_offset);
-//     uint8 locals[locals_size];
-//     fread(locals, sizeof(uint8), locals_size, tablemap_offset);
-//     // 対応するoffsetまで移動
-//     uint32 ooffset;
-//     uint64 type_table_addr, pre_type_table_addr;
-//     while(!feof(tablemap_offset)) {
-//        fread(&ooffset, sizeof(uint32), 1, tablemap_offset); 
-//        fread(&type_table_addr, sizeof(uint64), 1, tablemap_offset); 
-//        if (offset == ooffset) break;
-//        pre_type_table_addr = type_table_addr;
-//     }
-//     if (feof(tablemap_offset)) {
-//         perror("tablemap_offsetがおかしい\n");
-//         exit(1);
-//     }
-//     // type_table_addr = pre_type_table_addr;
-
-//     /// type_table
-//     fseek(type_table, type_table_addr, SEEK_SET);
-//     uint32 stack_size;
-//     fread(&stack_size, sizeof(uint32), 1, type_table);
-//     uint8 stack[stack_size];
-//     fread(stack, sizeof(uint8), stack_size, type_table);
-
-//     if (is_return_address) {
-//         fread(&stack_size, sizeof(uint32), 1, type_table);
-//         fread(stack, sizeof(uint8), stack_size, type_table);
-//     }
-
-//     // uint8 type_stack[locals_size + stack_size];
-//     uint8* type_stack = malloc(locals_size + stack_size);
-//     for (uint32 i = 0; i < locals_size; ++i) type_stack[i] = locals[i];
-//     for (uint32 i = 0; i < stack_size; ++i) type_stack[locals_size + i] = stack[i];
-
-//     // printf("new type stack: [");
-//     // for (uint32 i = 0; i < locals_size + stack_size; ++i) {
-//     //     if (i+1 == locals_size + stack_size)printf("%d", type_stack[i]);
-//     //     else                                printf("%d, ", type_stack[i]);
-//     // }
-//     // printf("]\n");
-
-//     fclose(tablemap_func);
-//     fclose(tablemap_offset);
-//     fclose(type_table);
-
-//     *type_stack_size = locals_size + stack_size;
-//     return type_stack;
-// }
 
 /* wasm_dump */
 static void
@@ -185,53 +91,32 @@ _dump_stack(WASMExecEnv *exec_env, struct WASMInterpFrame *frame, uint32 call_st
     int i;
     WASMModuleInstance *module = exec_env->module_inst;
 
-    // Entry function
-    // wasm_dump_stackの方でdump
-
     // リターンアドレス
     // NOTE: 1番下のframeのときだけ、prev_frameではなくframeのリターンアドレスを出力する
     WASMInterpFrame* prev_frame = (frame->prev_frame->function ? frame->prev_frame : frame);
     CodePos ret_addr;
     ret_addr.fidx = prev_frame->function - module->e->functions;
     ret_addr.offset = prev_frame->ip - wasm_get_func_code(prev_frame->function);
-    // fwrite(&fidx, sizeof(uint32), 1, fp);
-    // fwrite(&offset, sizeof(uint32), 1, fp);
-
-    // 型スタックのサイズ
-    WASMFunctionInstance *func = frame->function;
-    uint32 locals = func->param_count + func->local_count;
-    // uint32 type_stack_size = (frame->tsp - frame->tsp_bottom);
-    // uint32 full_type_stack_size = type_stack_size + locals;
-    // fwrite(&full_type_stack_size, sizeof(uint32), 1, fp);
 
     // 型スタックの中身
     uint32 type_stack_size_from_file;
     CodePos cur_addr;
     cur_addr.fidx = frame->function - module->e->functions;
     cur_addr.offset = frame->ip - wasm_get_func_code(frame->function);
-    // printf("[DEBUG]now addr: (%d, %d)\n", fidx_now, offset_now);
-    // uint8* type_stack_from_file = get_type_stack(fidx_now, offset_now, &type_stack_size_from_file, !is_top);
-    // fwrite(&type_stack_size_from_file, sizeof(uint32), 1, fp);
-    // fwrite(type_stack_from_file, sizeof(uint8), type_stack_size_from_file, fp);
-    // free(type_stack_from_file);
 
     // 値スタックの中身
-    uint32 local_cell_num = func->param_cell_num + func->local_cell_num;
+    WASMFunctionInstance *func = frame->function;
+    uint32 local_size = func->param_cell_num + func->local_cell_num;
     uint32 value_stack_size = frame->sp - frame->sp_bottom;
-    uint32 values[local_cell_num + value_stack_size];
-    memcpy(values, frame->lp, local_cell_num);
-    memcpy(values+local_cell_num, frame->sp_bottom, value_stack_size);
+    uint32 values[local_size + value_stack_size];
+    memcpy(values, frame->lp, local_size);
+    memcpy(values+local_size, frame->sp_bottom, value_stack_size);
     Array32 value_stack;
-    value_stack.size = local_cell_num+value_stack_size;
+    value_stack.size = local_size+value_stack_size;
     value_stack.contents = values;
-    // fwrite(frame->lp, sizeof(uint32), local_cell_num, fp);
-    // fwrite(frame->sp_bottom, sizeof(uint32), value_stack_size, fp);
-
-    // ラベルスタックのサイズ
-    uint32 ctrl_stack_size = frame->csp - frame->csp_bottom;
-    // fwrite(&ctrl_stack_size, sizeof(uint32), 1, fp);
 
     // ラベルスタックの中身
+    uint32 ctrl_stack_size = frame->csp - frame->csp_bottom;
     uint32_t begins[ctrl_stack_size];
     uint32_t targets[ctrl_stack_size];
     uint32_t stack_pointers[ctrl_stack_size];
@@ -241,21 +126,10 @@ _dump_stack(WASMExecEnv *exec_env, struct WASMInterpFrame *frame, uint32 call_st
     uint32 addr;
     uint8* ip_start = wasm_get_func_code(frame->function);
     for (i = 0; i < ctrl_stack_size; ++i, ++csp) {
-        // uint8 *begin_addr;
         begins[i] = get_addr_offset(csp->begin_addr, ip_start);
-        // fwrite(&addr, sizeof(uint32), 1, fp);
-
-        // uint8 *target_addr;
         targets[i] = get_addr_offset(csp->target_addr, ip_start);
-        // fwrite(&addr, sizeof(uint32), 1, fp);
-
-        // uint32 *frame_sp;
         stack_pointers[i] = get_addr_offset(csp->frame_sp, frame->sp_bottom);
-        // fwrite(&addr, sizeof(uint32), 1, fp);
-
-        // uint32 cell_num;
         cell_nums[i] = csp->cell_num;
-        // fwrite(&csp->cell_num, sizeof(uint32), 1, fp);
     }
 
     LabelStack labels;
@@ -265,6 +139,7 @@ _dump_stack(WASMExecEnv *exec_env, struct WASMInterpFrame *frame, uint32 call_st
     labels.stack_pointers = stack_pointers;
     labels.cell_nums = cell_nums;
 
+    // dump stack
     uint32 entry_fidx = frame->function - module->e->functions;
     bool is_top = (bool)(call_stack_id == 1);
     checkpoint_stack(call_stack_id, entry_fidx, &ret_addr, &cur_addr, &value_stack, &labels, is_top);
@@ -278,21 +153,12 @@ wasm_dump_stack(WASMExecEnv *exec_env, struct WASMInterpFrame *frame)
         (WASMModuleInstance *)exec_env->module_inst;
 
     // frameをtopからbottomまで走査する
-    char file[32];
     int i = 0;
     do {
         // dummy framenならbreak
         if (frame->function == NULL) break;
-
         ++i;
-        // sprintf(file, "stack%d.img", i);
-        // FILE *fp = open_image(file, "wb");
-
-        // uint32 entry_fidx = frame->function - module->e->functions;
-        // fwrite(&entry_fidx, sizeof(uint32), 1, fp);
-
         _dump_stack(exec_env, frame, i);
-        // fclose(fp);
     } while(frame = frame->prev_frame);
 
     // frame stackのサイズを保存
